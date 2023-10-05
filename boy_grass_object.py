7ac4fc5b175f64b496078d79df548393b651ed5c
TIP209_21 <11@11>
2023-10-05 13:11:23 +0900

    Update boy_grass_object.py

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 
 
 # Game object class here
@@ -14,8 +15,8 @@ class Grass:
 
 class Boy:
     def __init__(self):
-        self.x, self.y = 0, 90
-        self.frame = 0
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
         self.image = load_image('run_animation.png')
 
     def update(self):
@@ -24,6 +25,38 @@ class Boy:
 
     def draw(self):
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+
+class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 599
+        self.speed = random.randint(5, 10)
+        self.type = random.randint(0, 1)
+        if (self.type):
+            self.image = load_image('ball21x21.png')
+        else:
+            self.image = load_image('ball41x41.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+
+        if self.type == 1:
+            if self.y > 60:
+                self.y -= self.speed
+
+            if self.y < 60:
+                self.y = 60
+            else:
+                pass
+        else:
+            if self.y > 70:
+                self.y -= self.speed
+            if self.y < 70:
+                self.y = 70
+            else:
+                pass
 
 
 def handle_events():
@@ -39,23 +72,34 @@ def handle_events():
 def reset_world():
     global running
     global grass
-    global boy
+    global team
+    global world
+    global balls
 
     running = True
+    world = []
+
     grass = Grass()  # 클래스를 이용해 객체를 찍어냄
-    boy = Boy()
+    world.append(grass)
+
+    team = [Boy() for i in range(10)]
+    world += team
+
+    balls = [Ball() for i in range(20)]
+    world += balls
 
 
 def update_world():
     grass.update()
-    boy.update()
-    pass
+    for o in world:
+        o.update()
 
 
 def render_world():
     clear_canvas()
     grass.draw()
-    boy.draw()
+    for o in world:
+        o.draw()
     update_canvas()
 
 
